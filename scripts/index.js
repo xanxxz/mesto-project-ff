2909c1d968e4b1e41d3b7710f4d92bf085319005
unknown <boneislandman00@gmail.com>
2025-02-18 15:19:38 +0300

    first-commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,36 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.getElementById('card-template');
+const placesList = document.querySelector('.places__list');
 
-// @todo: DOM узлы
+function createCard(cardData, removeCard) {
+  const cardElement = cardTemplate.content.cloneNode(true);
 
-// @todo: Функция создания карточки
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+  const deleteButton = cardElement.querySelector('.card__delete-button');
 
-// @todo: Функция удаления карточки
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  cardTitle.textContent = cardData.name;
 
-// @todo: Вывести карточки на страницу
+  deleteButton.addEventListener('click', () => {
+    removeCard();
+  });
+
+  return cardElement;
+}
+
+function renderCards(cards) {
+  cards.forEach(cardData => {
+    const cardElement = createCard(cardData, removeCard);
+
+    placesList.appendChild(cardElement);
+  });
+}
+
+function removeCard() {
+  const cardElement = document.querySelector('.card');
+  
+  cardElement.remove();
+}
+
+renderCards(initialCards);
